bdbac077df209466bc9bfc683ce0fdbfe59f31b6
worldofpaper <worldofpaper@b091eaf8-b234-11dd-b680-ffd21d2dd7b0>
2010-01-07 13:24:20 +0000

    Refactored the core packages a little - bit better organised now.

--- trunk/core/src/org/brainfarm/java/api/ISpringXMLCapable.java
+++
@@ -1,9 +0,0 @@
-package org.brainfarm.java.api;
-
-import org.w3c.dom.Document;
-import org.w3c.dom.Node;
-
-public interface ISpringXMLCapable {
-	
-	Node toSpringXML(Document xml);
-}
--- trunk/core/src/org/brainfarm/java/neat/NodeLabel.java
+++
@@ -1,8 +0,0 @@
-package org.brainfarm.java.neat;
-
-public enum NodeLabel {
-	HIDDEN,
-	INPUT,
-	OUTPUT,
-	BIAS
-}
